2da26ae54eee0ae03f07af5688b87746788f80ba
SamGilroy <65417959+SamGilroy@users.noreply.github.com>
2021-05-29 15:58:31 -0400

    Update groceries.js

--- SEG_3125-Lab_2/scripts/groceries.js
+++ SEG_3125-Lab_2/scripts/groceries.js
@@ -89,10 +89,7 @@ var products = [
 function restrictListProducts(prods, restriction) {
 	let product_names = [];
 	for (let i=0; i<prods.length; i+=1) {
-		if ((restriction.includes("nutAllergy")) && (restriction.includes("lactoseIntolerant")) && (prods[i].lactoseIntolerant == true) && (restriction.includes("organic")) && (prods[i].organic == true)){
-			product_names.push(prods[i]);
-		}
-		else if ((restriction.includes("nutAllergy")) && (restriction.includes("lactoseIntolerant")) && (prods[i].lactoseIntolerant == true) && (prods[i].nutAllergy == true)){
+		if ((restriction.includes("nutAllergy")) && (restriction.includes("lactoseIntolerant")) && (prods[i].lactoseIntolerant == true) && (prods[i].nutAllergy == true)){
 			product_names.push(prods[i]);
 		}
 		else if ((restriction.includes("lactoseIntolerant")) && !(restriction.includes("nutAllergy")) && (prods[i].lactoseIntolerant == true)){
