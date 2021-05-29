004ae2dc04dbd4850943c1a936ca6608962f12cf
SamGilroy <65417959+SamGilroy@users.noreply.github.com>
2021-05-29 16:59:34 -0400

    Update main.js

--- SEG_3125-Lab_2/scripts/main.js
+++ SEG_3125-Lab_2/scripts/main.js
@@ -39,16 +39,28 @@ function populateListProductChoices(c1,c2,c3, slct2) {
     
     var s2 = document.getElementById(slct2);
 	
-	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty	
+	
     s2.innerHTML = "";
 		
-	// obtain a reduced list of products based on restrictions
-    var optionArray = restrictListProducts(products, chosenPrefs);
+    // obtain a reduced list of products based on restrictions
+    var optionArray = restrictListProducts(products, s1);
+    
+    // Sort with comparator to sort products by price.
+    optionArray.sort(function(a,b){
+        if(products[a].price < products[b].price) {return -1;}     	
+        else if(products[a].price > products[b].price) {return 1;}
+        else{return 0;}
+    });
+
+	// for each item in the array, create a checkbox element, each containing information such as:
+	// <input type="checkbox" name="product" value="Bread">
+	// <label for="Bread">Bread/label><br>
 		
-	for (let i = 0; i < optionArray.length; i++) {
+	for (i = 0; i < optionArray.length; i++) {
 			
-		var productName = optionArray[i].name;
-		var productPrice = optionArray[i].price;
+		var productName = products[optionArray[i]].name;
+		var pricey = products[optionArray[i]].price;
 		// create the checkbox and add in HTML DOM
 		var checkbox = document.createElement("input");
 		checkbox.type = "checkbox";
@@ -59,11 +71,20 @@ function populateListProductChoices(c1,c2,c3, slct2) {
 		// create a label for the checkbox, and also add in HTML DOM
 		var label = document.createElement('label')
 		label.htmlFor = productName;
-		label.appendChild(document.createTextNode(productName + " $" + productPrice));
+		label.appendChild(document.createTextNode(productName + " - " + pricey));
 		s2.appendChild(label);
 		
 		// create a breakline node and add in HTML DOM
-		s2.appendChild(document.createElement("br"));    
+		s2.appendChild(document.createElement("br"));
+		
+		// Show a picture of the item and add an extra space.
+		var image = document.createElement("img");
+		image.src = products[optionArray[i]].img;
+		image.alt = productName;
+		s2.appendChild(image);
+		
+		s2.appendChild(document.createElement("br"));
+		s2.appendChild(document.createElement("br"));
 	}
 }
 	
