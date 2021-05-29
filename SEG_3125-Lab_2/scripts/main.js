87064dbb2851fbd6affdd3dbdbc404f7171a58e3
SamGilroy <65417959+SamGilroy@users.noreply.github.com>
2021-05-29 19:33:04 -0400

    Update main.js

--- SEG_3125-Lab_2/scripts/main.js
+++ SEG_3125-Lab_2/scripts/main.js
@@ -93,7 +93,7 @@ function selectedItems(){
 	
 	// build list of selected item
 	var para = document.createElement("P");
-	para.innerHTML = "Your order is as follows:";
+	para.innerHTML = "Your order: ";
 	para.appendChild(document.createElement("br"));
 	para.appendChild(document.createElement("br"));
 	for (i = 0; i < ele.length; i++) { 
@@ -103,8 +103,8 @@ function selectedItems(){
 			chosenProducts.push(ele[i].value);
 		}
 	}
+		
 	// add paragraph and total price
 	c.appendChild(para);
-	c.appendChild(document.createTextNode("Total Price is $" + getTotalPrice(chosenProducts)));
-		
+	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));		
 }
