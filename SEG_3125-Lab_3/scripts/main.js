afd9e1498fdce800d3d7077170860b4497929e8e
SamGilroy <65417959+SamGilroy@users.noreply.github.com>
2021-06-04 17:27:38 -0400

    Update main.js

--- SEG_3125-Lab_3/scripts/main.js
+++ SEG_3125-Lab_3/scripts/main.js
@@ -124,10 +124,28 @@ for (i = 0; i < optionArray.length; i++) {
 // The purpose is to build the HTML to be displayed (a Paragraph) 
 // We build a paragraph to contain the list of selected items, and the total price
 
-// Modified to comply with updated cart.
 function selectedItems(){
-
-	var q = document.getElementById('totCounter');
-	q.innerHTML = "Total: $" + getTotalPrice();
+	
+	var ele = document.getElementsByName("product");
+	var chosenProducts = [];
+	
+	var c = document.getElementById('displayCart');
+	c.innerHTML = "";
+	
+	// build list of selected item
+	var para = document.createElement("P");
+	para.innerHTML = "Your order is the following: ";
+	para.appendChild(document.createElement("br"));
+	para.appendChild(document.createElement("br"));
+	for (i = 0; i < ele.length; i++) { 
+		if (ele[i].checked) {
+			para.appendChild(document.createTextNode(ele[i].value));
+			para.appendChild(document.createElement("br"));
+			chosenProducts.push(ele[i].value);
+		}
+	}
 		
+	// add paragraph and total price
+	c.appendChild(para);
+	c.appendChild(document.createTextNode("Total Price is $" + getTotalPrice(chosenProducts).toFixed(2)));		
 }
