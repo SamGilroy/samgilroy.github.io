4bb3ccbbfe8906a87ccfca31434047539090f1a6
SamGilroy <65417959+SamGilroy@users.noreply.github.com>
2021-06-04 18:03:03 -0400

    Update main.js

--- SEG_3125-Lab_3/scripts/main.js
+++ SEG_3125-Lab_3/scripts/main.js
@@ -70,8 +70,8 @@ function populateListProductChoices(c1,c2,c3, slct2) {
     });
 
 for (i = 0; i < optionArray.length; i++) {
-	
-	   var productName = products[optionArray[i]].name;
+			
+		var productName = products[optionArray[i]].name;
 		var price = products[optionArray[i]].price;
 		// create the checkbox and add in HTML DOM
 		var checkbox = document.createElement("input");
@@ -83,20 +83,13 @@ for (i = 0; i < optionArray.length; i++) {
 		// create a label for the checkbox, and also add in HTML DOM
 		var label = document.createElement('label')
 		label.htmlFor = productName;
-		label.appendChild(document.createTextNode(productName + " -$" + price.toFixed(2)));
+		label.appendChild(document.createTextNode(productName + " - $" + price.toFixed(2)));
 		s2.appendChild(label);
 		
 		// create a breakline node and add in HTML DOM
 		s2.appendChild(document.createElement("br"));
-		
-		// Show a picture of the item and add an extra space.
-		var image = document.createElement("img");
-		image.src = products[optionArray[i]].img;
-		image.alt = productName;
-		s2.appendChild(image);
-		
 		s2.appendChild(document.createElement("br"));
-		s2.appendChild(document.createElement("br"));   
+		s2.appendChild(document.createElement("br"));
 	}
 }
 	
