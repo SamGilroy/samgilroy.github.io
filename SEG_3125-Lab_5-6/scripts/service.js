b9e8d68576b79d8fa59752f19dd671cfe89b1943
SamGilroy <65417959+SamGilroy@users.noreply.github.com>
2021-06-13 22:43:37 -0400

    Update service.js

--- SEG_3125-Lab_5-6/scripts/service.js
+++ SEG_3125-Lab_5-6/scripts/service.js
@@ -12,7 +12,7 @@ function validatePhone(txtPhone) {
 }
 
 function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /\S+@\S+\.\S+/;
     return re.test(String(email).toLowerCase());
 }
 
