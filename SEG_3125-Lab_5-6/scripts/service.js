812c83987ffa2fee929c2a80b29bed46d3adcb08
SamGilroy <65417959+SamGilroy@users.noreply.github.com>
2021-06-13 23:34:15 -0400

    Update service.js

--- SEG_3125-Lab_5-6/scripts/service.js
+++ SEG_3125-Lab_5-6/scripts/service.js
@@ -58,7 +58,7 @@ $(document).ready(function(){
     
     $("#email").on("change", function(){
         if (!validateEmail("email")){
-            alert("e");
+            alert("Please use the proper format.");
             $("#email").val("");
             $("#email").addClass("error");
         }
