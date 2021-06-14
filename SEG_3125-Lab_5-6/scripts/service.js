4122258bc368baf50bf4094b8f07bf9f6ba8da73
SamGilroy <65417959+SamGilroy@users.noreply.github.com>
2021-06-13 21:04:30 -0400

    Update service.js

--- SEG_3125-Lab_5-6/scripts/service.js
+++ SEG_3125-Lab_5-6/scripts/service.js
@@ -2,7 +2,7 @@ function validatePhone(txtPhone) {
     var a = document.getElementById(txtPhone).value;
     // This filter asks for something like (12345), so parentheses with any number (at least 1)
     // of digits
-    var filter = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    var filter = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
     if (filter.test(a)) {
         return true;
     }
