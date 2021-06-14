d0d080e282abad635fb39bc5f17e39a42a41a479
SamGilroy <65417959+SamGilroy@users.noreply.github.com>
2021-06-13 23:09:36 -0400

    Update service.js

--- SEG_3125-Lab_5-6/scripts/service.js
+++ SEG_3125-Lab_5-6/scripts/service.js
@@ -11,7 +11,7 @@ function validatePhone(txtPhone) {
 
 function validateEmail(email) {
     var b = document.getElementById(email).value;
-    var re = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(b)) {
         return true;
     }
